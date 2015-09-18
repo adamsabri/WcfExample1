25c6502fa2bcc825d07540e5a0e948cfdf72875a
adamsabri <engineer.software2014@gmail.com>
2015-09-18 09:47:58 +0100

    commit web service Self Hosting

---
+++ RestHostConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using WcfService;
+
+namespace RestHostConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServiceHost();
+
+            //ServiceHost();
+        }
+
+        private static void ServiceHost()
+        {
+            //Create two different URIs to serve as the base address
+            // One for http requests and another for net.tcp
+            var httpUrl = new Uri("http://localhost:8733/WcfService/RestService");
+            //var netTcpUrl = new Uri("net.tcp://localhost:8080/RestService");
+
+            //Create ServiceHost to host the service in the console application
+            var host = new ServiceHost(typeof(RestService), httpUrl);
+
+            //Enable metadata exchange - you need this so others can create proxies
+            //to consume your WCF service
+            //var serviceMetaBehavior = new ServiceMetadataBehavior { HttpGetEnabled = true };
+            //host.Description.Behaviors.Add(serviceMetaBehavior);
+
+            //Start the Service
+            host.Open();
+
+            foreach (ServiceEndpoint se in host.Description.Endpoints)
+                Console.WriteLine("Service is host with endpoint " + se.Address);
+            Console.WriteLine("Host is running... Press <Enter> key to stop");
+            Console.ReadKey();
+        }
+
+        private static void WebServiceHost()
+        {
+
+            //var httpUrl = new Uri("http://localhost:8090/MyService/EmployeeService");
+            var httpUrl = new Uri("http://localhost:8090/RestService");
+            var host = new WebServiceHost(typeof(RestService), httpUrl);
+            host.Open();
+
+            foreach (ServiceEndpoint se in host.Description.Endpoints)
+                Console.WriteLine("Service is host with endpoint " + se.Address);
+            //Console.WriteLine("ASP.Net : " + ServiceHostingEnvironment.AspNetCompatibilityEnabled);
+            Console.WriteLine("Host is running... Press < Enter > key to stop");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfService/IRestService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IRestService
+    {
+        //[OperationContract]
+        ////[WebGet(ResponseFormat = WebMessageFormat.Xml, UriTemplate = "GetProductList/")]
+        ////[WebGet(UriTemplate = "{0}/GetProductList/")]
+        //[WebGet]
+        //List<Product> GetProductList();
+
+        [WebGet(UriTemplate = "Employee", RequestFormat = WebMessageFormat.Xml)]
+        [OperationContract]
+        List<Employee> GetAllEmployeeDetails();
+
+        [WebGet(UriTemplate = "Employee?id={id}")]
+        [OperationContract]
+        Employee GetEmployee(int id);
+
+        [WebInvoke(Method = "POST", UriTemplate = "EmployeePOST")]
+        [OperationContract]
+        void AddEmployee(Employee newEmp);
+        
+        [WebInvoke(Method = "PUT", UriTemplate = "EmployeePUT")]
+        [OperationContract]
+        void UpdateEmployee(Employee newEmp);
+
+        [WebInvoke(Method = "DELETE", UriTemplate = "Employee/{empId}")]
+        [OperationContract]
+        void DeleteEmployee(string empId);
+    }
+
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string CategoryName { get; set; }
+
+        [DataMember]
+        public int Price { get; set; }
+    }
+
+    public class Products
+    {
+        private static readonly Products _instance = new Products();
+        private static readonly List<Product> _products = new List<Product>
+            {
+                new Product{ProductId = 1, Name = "Prod 1", CategoryName = "Cat 1", Price = 10},
+                new Product{ProductId = 2, Name = "Prod 2", CategoryName = "Cat 2", Price = 15}
+            };
+
+        public static Products Instance
+        {
+            get { return _instance; }
+        }
+
+        private Products()
+        {
+        }
+
+        public List<Product> ProductList
+        {
+            get { return _products; }
+        }
+
+    }
+}
